48bc53c4b3679c5e301f8f8e0907c427c9376ae0
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-31 11:45:20 -0300

    build: create database storage for users with nedb

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,10 @@
+let NeDB = require('nedb');
+
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) => {
     app.get('/users', (req, res) => { 
         res.status(200).json({ 
@@ -16,5 +23,15 @@ module.exports = (app) => {
     });
 
     app.post('/users/admin', (req, res) => {       
-            res.json(req.body)
-        })};
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`Error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        });    
+    });
+};
