22bb86cb3308d841f2ebee38a6fe904f2b9f642d
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-31 10:36:33 -0300

    feat: add switch and array users with JSON

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const http = require('http');
 
 let server = http.createServer((req, res) => {
 
-    console.log('URL:', req.url)
+    console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    res.end('Ok');
+    switch(req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>RODANDO</h1>');
+            break;
+
+        case '/users': 
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 200;
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'juan',
+                    email: 'contato@getHeapCodeStatistics.com.br',
+                    id: 1
+                }]
+            }));
+            break; 
+          
+        default:
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Página não encontrada');
+    }
 });
 
-server.listen(3000, '127.0.0.1', ()=> {
-    console.log('rodando')
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Servidor rodando em http://127.0.0.1:3000');
 });
