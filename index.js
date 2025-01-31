89887aeea33a4659758d132ea8c5aa9ae074f250
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-31 10:52:20 -0300

    refactor: migrate server from Node.js HTTP module to Express.js

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-const http = require('http');
+const express = require('express'); 
+const app = express(); 
 
-let server = http.createServer((req, res) => {
-
+app.get('/', (req, res) => { 
     console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    switch(req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>RODANDO</h1>');
-            break;
-
-        case '/users': 
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'juan',
-                    email: 'contato@getHeapCodeStatistics.com.br',
-                    id: 1
-                }]
-            }));
-            break; 
-          
-        default:
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Página não encontrada');
-    }
+    res.status(200).setHeader('Content-Type', 'text/html').end('<h1>RODANDO</h1>');
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => { 
+    res.status(200).json({ 
+        users: [{ 
+            name: 'juan',
+            email: 'contato@getHeapCodeStatistics.com.br',
+            id: 1
+        }]
+    });
+});
+
+app.listen(3000, () => {
     console.log('Servidor rodando em http://127.0.0.1:3000');
 });
