9fbec6964d6bac20951e8d37293af69d7fcd2de9
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-02-03 10:35:23 -0300

    feat: add crud for users (edit, delete)

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-const { request } = require('express');
 let NeDB = require('nedb');
 
 let db = new NeDB({
@@ -11,30 +10,21 @@ module.exports = (app) => {
         .get((req, res) => { 
             db.find({}).sort({ name: 1 }).exec((err, users) => {
                 if (err) {
-                    if (app.utils && app.utils.error && typeof app.utils.error.send === 'function') {
-                        app.utils.error.send(err, req, res);
-                    } else {
-                        res.status(400).json({ error: err });
-                    }
-                } else {
-                    res.status(200).json({
-                        users: users
-                    });
+                    return res.status(400).json({ error: err });
                 }
+                res.status(200).json({ users });
             });
         })
         .post((req, res) => {
+            if (!req.body.name || !req.body.email) {
+                return res.status(400).json({ error: 'Nome e e-mail são obrigatórios' });
+            }
+
             db.insert(req.body, (err, user) => {
                 if (err) {
-                    console.log(`Error: ${err}`);
-                    if (app.utils && app.utils.error && typeof app.utils.error.send === 'function') {
-                        app.utils.error.send(err, req, res);
-                    } else {
-                        res.status(400).json({ error: err });
-                    }
-                } else {
-                    res.status(200).json(user);
+                    return res.status(400).json({ error: err });
                 }
+                res.status(201).json(user);
             });
         });
 
@@ -45,35 +35,42 @@ module.exports = (app) => {
             });
         })
         .post((req, res) => {       
-            db.insert(req.body, (err, user) => {
+            if (!req.body.name || !req.body.email) {
+                return res.status(400).json({ error: 'Nome e e-mail são obrigatórios' });
+            }
+
+            let newUser = { ...req.body, role: 'admin' };
+            db.insert(newUser, (err, user) => {
                 if (err) {
-                    console.log(`Error: ${err}`);
-                    if (app.utils && app.utils.error && typeof app.utils.error.send === 'function') {
-                        app.utils.error.send(err, req, res);
-                    } else {
-                        res.status(400).json({ error: err });
-                    }
-                } else {
-                    res.status(200).json(user);
+                    return res.status(400).json({ error: err });
                 }
+                res.status(201).json(user);
             });    
         });
 
-        let routeId = app.route('/users/:id');
-
-        routeId.get((req, res) => {
-            db.findOne({_id: req.params.id}).exec((err, user) => {  // Aqui usei req.params.id
-                if (err) {
-                    console.log(`Error: ${err}`);
-                    if (app.utils && app.utils.error && typeof app.utils.error.send === 'function') {
-                        app.utils.error.send(err, req, res);
-                    } else {
-                        res.status(400).json({ error: err });
-                    }
-                } else {
-                    res.status(200).json(user);
+    app.route('/users/:id')
+        .get((req, res) => {
+            db.findOne({ _id: req.params.id }).exec((err, user) => { 
+                if (err || !user) {
+                    return res.status(404).json({ error: 'Usuário não encontrado' });
                 }
-            })
+                res.status(200).json(user);
+            });
+        })
+        .put((req, res) => {
+            db.update({ _id: req.params.id }, { $set: req.body }, {}, (err, numReplaced) => {
+                if (err) {
+                    return res.status(400).json({ error: err });
+                }
+                res.status(200).json({ message: "Usuário atualizado com sucesso", numReplaced });
+            });
+        })
+        .delete((req, res) => {
+            db.remove({ _id: req.params.id }, {}, (err, numRemoved) => {
+                if (err) {
+                    return res.status(400).json({ error: err });
+                }
+                res.status(200).json({ message: "Usuário removido com sucesso", numRemoved });
+            });
         });
-        
 };
