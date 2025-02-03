7efa3e92e70f431b600933afe6d90f8d0e978945
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-02-03 11:12:38 -0300

    build: add config for express validator

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const app = express(); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
